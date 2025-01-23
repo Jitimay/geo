91d0ceca300d74e9e39123bbe89c086672bb57f7
Jitimay <jitimayjosh1@gmail.com>
2025-01-23 07:40:06 -0700

    Initial commit

---
+++ lib/bloc/geo_bloc_event.dart
@@ -0,0 +1,12 @@
+//
+// import 'package:equatable/equatable.dart';
+// part of 'geo_bloc_bloc.dart';
+//
+// sealed class GeoBlocEvent extends Equatable{
+//   const GeoBlocEvent();
+//
+//   @override
+//   List<Object> get props => [];
+// }
+//
+// class FetchGeo extends GeoBloc
